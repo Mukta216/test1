bce05a994a579ef8207a728359077577d8cc556a
Mitali <LehagaonkarMukta@gmail.com>
2024-05-21 22:58:36 -0700

    change in project code commit and push

--- SB_Java_lecture/src/com/codeo/Operators/ArithmaticOperators.java
+++ SB_Java_lecture/src/com/codeo/Operators/ArithmaticOperators.java
@@ -3,8 +3,8 @@ package com.codeo.Operators;
 public class ArithmaticOperators {
 
 	public static void main(String[] args) {
-		int a=18;
-		int b=12;
+		int a=17;
+		int b=15;
 		int c=a+b;
 		System.out.println(c);
 		System.out.println(a-b);
